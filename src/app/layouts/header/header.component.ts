d8b33fc6465977fb87e36abfdbb37993a55783a6
nmreddy1911 <manishreddy2000n@gmail.com>
2021-05-16 20:23:20 +0530

    env

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   constructor() {}
   src = 'https://i.ibb.co/jJB2b0h/todo.png';
-  
-  ngOnInit(): void {}
 }
